778e34fb4b5ce2d783c1fcdace0d2251db0351f5
Yavuz Gedik <yavuzgedik@outlook.com>
2016-10-29 21:32:04 +0300

    Added Class and Object

---
+++ Project01/Project01/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project01
+{
+    public class Baby
+    {
+        //Baby Nesnesinin Özellikleri
+        public string Name;
+        public string Surname;
+        public int Age;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Baby b = new Baby();
+            b.Name = "Yavuz";
+            b.Surname = "Gedik";
+            b.Age = 3;
+            
+            Console.WriteLine("İsim Soyisim\t: {0} {1}", b.Name,b.Surname);
+            Console.WriteLine("Yaş\t\t: {0}", b.Age);
+
+            Console.ReadKey();
+        }
+    }
+}
